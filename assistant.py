bf156660688fe3eda988aadffb7a4e06a7fa1b6e
hanzo-hasashi-x <iusan7756@gmail.com>
2024-11-06 15:30:41 +0100

    2nd part

--- assistant.py
+++ assistant.py
@@ -1,4 +1,7 @@
+import re
+
 filename = None
+words = None
 
 def file_cmd(command_str):
     """
@@ -22,19 +25,90 @@ def file_cmd(command_str):
 
         return f'Loaded {filename}'
     except FileNotFoundError:
+        filename = None
         return 'Error because file not found' 
     except:
         return 'Uknown exception'
 
 
 def info_cmd(command_str):
-    pass
+    """
+    pre: `command_str`: str of format `file <file>`
+         `filename` initialized by `file_cmd`
+    post: returns :str
+          - Either the message that informs that the number of lines and the number of characters 
+          - Or the message with exception, if there exists some troubles with file or format
+    """
+    global filename
+
+    if not filename:
+        return 'You haven\'t forgot to enter the file name'
+
+    try:
+        with open(filename, 'r') as f: # checks the possibility to load the file
+            lines = f.readlines()
+            chars_num = sum(len(line) for line in lines)
+            return f'{len(lines)} lines\n{chars_num}'
+    except:
+        return 'Uknown exception'
 
 def words_cmd(command_str):
-    pass
+    """
+    pre: `command_str`: str of format `words`
+         `filename` initialized by `file_cmd`
+    post: returns :str
+          - Either the message that informs that file was found
+          - Or the message with exception, if there exists some troubles with file or format
+    """
+    global words, filename
+
+    if not filename:
+        return 'You haven\'t forgot to enter the file name'
+
+    try:
+        with open(filename, 'r') as f: # checks the possibility to load the file
+            words = []
+            for line in f.readlines():
+                word = line.split(',')[0]
+
+                if re.findall('[0-9]', word):
+                    return 'File contains not only the letters, but also numbers'
+
+                words.append(word)
+
+        return 'Read file as list of words'
+    except:
+        return 'Uknown exception'
 
 def search_cmd(command_str):
-    pass
+    """
+    pre: `command_str`: str of format `search <word>`
+         `words` initialized by `words_cmd`
+    post: returns :str
+          - Either the message that informs that word was found or not
+          - Or the message with exception, if there exists some troubles with search
+    """
+    global words
+
+    args = command_str.split(' ')[1:]
+
+    if len(args) == 0:
+        return 'You haven\'t entered the numbers'
+    word = args[0]
+
+    if re.findall('[0-9]', word):
+        return 'You word contains numbers'
+
+    if not words:
+        return 'You haven\'t forgot to enter the file name and/or push it as words list'
+
+    try:
+        if word in words:
+            return f'{word} is in the list of words'
+        else:
+            return f'{word} isn\'t in the list of words'
+    except:
+        return 'Uknown exception'
 
 def sum_cmd(command_str):
     """
--- tests.py
+++ tests.py
@@ -6,13 +6,18 @@ def test_file_cmd():
     assert file_cmd('file') == 'You haven\'t entered the file name'
 
 def test_info_cmd():
-    pass
+    file_cmd('file all-words.dat')
+    assert info_cmd('info') == '113004 lines\n1330218'
 
 def test_words_cmd():
-    pass
+    file_cmd('file all-words.dat')
+    assert words_cmd('words') == 'Read file as list of words'
 
 def test_search_cmd():
-    pass
+    file_cmd('file all-words.dat')
+    info_cmd('words')
+    assert search_cmd('search joker') == 'joker is in the list of words'
+    # assert 'number' in search_cmd('search joker1')
 
 def test_sum_cmd():
     assert float(sum_cmd('sum 2 3 4 0.3 0.2').split(' ')[-1]) == sum([2,3,4,0.3,0.2])
@@ -30,14 +35,16 @@ def test_avg_cmd():
     assert 'Error' in avg_cmd('avg abc vac')
 
 def test():
+    cmd = None
     try:
         for cmd in [test_file_cmd, test_info_cmd, test_words_cmd, test_sum_cmd, test_avg_cmd]:
             cmd()
 
         print('All tests are passed without any exception.')
+
     except AssertionError as e:
-        print('The problem with output')
-        print(e)
+        print(f'The problem with output at {cmd}')
+
     except Exception as e:
         print('The problem that\'s not handled')
         print(e)
